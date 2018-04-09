b6ec8f8ab6bcc9c0076759bc4e73f3a405a743fb
johnCoder83 <33188152+johnCoder83@users.noreply.github.com>
2018-04-09 17:37:52 +0100

    Add files via upload

---
+++ TeamProject/assets/js/sidebarHandler.js
@@ -0,0 +1,18 @@
+//JOHN LEE HEANEY
+//LYIT Team Project 2018
+//BSc 5
+window.onload = function()
+{
+	openNav();
+	closeNav();
+}
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+
+}
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+
+
